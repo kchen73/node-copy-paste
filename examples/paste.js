f8d9a0c3a9afb8c8c1147d3123d9f487f100fa89
Kevin Chen <Kevin Chen>
2016-01-19 14:46:46 -0800

    added more comments

--- examples/paste.js
+++ examples/paste.js
@@ -1,9 +1,10 @@
+// Before running on command line, copy text externally
 'use strict';
 
 var cp = require('copy-paste');
 var text;
 
-// When executed on command line, second parameter of callback function is ready for pasting
+// When executed on command line, second parameter of callback function is ready for paste
 var paste = cp.paste(function(undefined, text) {
     console.log('You pasted --> ' + text);
 });
--- index.js
+++ index.js
@@ -5,18 +5,18 @@ var spawn = child_process.spawn;
 var util = require("util");
 
 /** 
+ * Loops to check if native execSync is available
  * @returns null
- * MORE COMMENTS NEEDED
- * IF/ELSE COMMENT W/ RETURNS NEEDED
  */
 var execSync = (function() {
-	if(child_process.execSync) { // Use native execSync if available
-		/** @returns  */
+    if(child_process.execSync) { 
+        /** @returns TBD */
         return function(cmd) { return child_process.execSync(cmd); };
 	} else {
 		try { // Try using fallback package if available
             /** Module imported to provide synchronous execution with status code support */
 			var execSync = require("sync-exec");
+            /** @returns TBD */
 			return function(cmd) { return execSync(cmd).stdout; };
 		} catch(e) {}
 	}
@@ -26,8 +26,8 @@ var execSync = (function() {
 var config;
 
 /**  
- * Determines which platform is prevalent.
- * Throws error if platform is unknown.
+ * Determines which platform is used
+ * Throws error if platform is unknown to module
  */
 switch(process.platform) {
 	case "darwin":
@@ -46,7 +46,9 @@ switch(process.platform) {
 		throw new Error("Unknown platform: '" + process.platform + "'.  Send this error to xavi.rmz@gmail.com.");
 }
 
-/** COMMENT NEEDED */
+/**
+ * no operation (do nothing) function
+ */
 var noop = function() {};
 
 /**
@@ -57,27 +59,45 @@ var noop = function() {};
  */
 exports.copy = function(text, callback) {
 	var child = spawn(config.copy.command, config.copy.args);
-
+    
+    /** Copy data to clipboard if callback exists, otherwise do nothing */
 	var done = (callback ? function() { callback.apply(this, arguments); done = noop; } : noop);
 
 	var err = [];
-
-	child.stdin.on("error", function (err) { done(err); });
+    
+	/**
+     * COMMENT NEEDED HERE
+     */
+    child.stdin.on("error", function (err) { done(err); });
 	child
+        /**
+         * COMMENT NEEDED HERE
+         */
 		.on("exit", function() { done(null, text); })
+        /**
+         * COMMENT NEEDED HERE
+         */
 		.on("error", function(err) { done(err); })
 		.stderr
+            /**
+             * Data is pushed to the err array
+             */
 			.on("data", function(chunk) { err.push(chunk); })
+            /**
+             * Tests whether there is data to be copied to clipboard
+             * @returns 'nothing'
+             */
 			.on("end", function() {
 				if(err.length === 0) { return; }
 				done(new Error(config.decode(err)));
 			})
 	;
-
+    /** Reads in text if work done on text is finished */
 	if(text.pipe) { text.pipe(child.stdin); }
 	else {
 		var output, type = Object.prototype.toString.call(text);
-
+        
+        /** Determines the data type that is copied and converts data into text format */
 		if(type === "[object String]") { output = text; }
 		else if(type === "[object Object]") { output = util.inspect(text, { depth: null }); }
 		else if(type === "[object Array]") { output = util.inspect(text, { depth: null }); }
@@ -89,28 +109,49 @@ exports.copy = function(text, callback) {
 	return text;
 };
 
+/** Chains the saved data located in the clipboard */
 var pasteCommand = [ config.paste.command ].concat(config.paste.args).join(" ");
+
 /** 
  * Pastes text from clipboard
  * @param callback
  */
 exports.paste = function(callback) {
-	if(execSync && !callback) { return config.decode(execSync(pasteCommand)); }
+	/** Pastes text from second parameter of callback function if callback exists */
+    /** Throws error if callback does not exist and execSync is not supported */
+    if(execSync && !callback) { return config.decode(execSync(pasteCommand)); }
 	else if(callback) {
 		var child = spawn(config.paste.command, config.paste.args);
 
-		var done = callback && function() { callback.apply(this, arguments); done = noop; };
+		/** Paste text from clipboard and perform no operation if callback function exists */
+        var done = callback && function() { callback.apply(this, arguments); done = noop; };
 
 		var data = [], err = [];
-
+        
+        /**
+         * COMMENT NEEDED HERE
+         */
 		child.on("error", function(err) { done(err); });
 		child.stdout
-			.on("data", function(chunk) { data.push(chunk); })
+			/**
+             * Data is pushed into the data array
+             */
+            .on("data", function(chunk) { data.push(chunk); })
+            /**
+             * Data is decoded
+             */
 			.on("end", function() { done(null, config.decode(data)); })
 		;
 		child.stderr
+            /**
+             * Data is pushed into the err array
+             */
 			.on("data", function(chunk) { err.push(chunk); })
-			.on("end", function() {
+			/**
+             * Tests whether clipboard contains data or not
+             * @returns 'nothing'
+             */
+            .on("end", function() {
 				if(err.length === 0) { return; }
 
 				done(new Error(config.decode(err)));
@@ -121,13 +162,24 @@ exports.paste = function(callback) {
 	}
 };
 
+/**
+ * If silent is called, throw deprecated error
+ */
 exports.silent = function() {
 	throw new Error("DEPRECATED: copy-paste is now always silent.");
 };
 
+/**
+ * If noConflict is called, throw deprecated error
+ */
 exports.noConflict = function() {
 	throw new Error("DEPRECATED: copy-paste no longer adds global variables by default.");
 };
+
+/**
+ * If global is called and chained to require("copy-paste"), copy and paste are added to global namespace
+ * @returns exports - public functions
+ */
 exports.global = function() {
 	GLOBAL.copy = exports.copy;
 	GLOBAL.paste = exports.paste;
